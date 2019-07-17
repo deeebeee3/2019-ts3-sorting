5c42f9a695d25499c8d1a06087c726d008ff5b8e
Deepak Bhari <deepakbhari@Deepaks-MBP.lan>
2019-07-17 17:25:52 +0100

    FIX typo with swap method of NumbersCollection

--- src/NumbersCollection.ts
+++ src/NumbersCollection.ts
@@ -17,7 +17,7 @@ export class NumbersCollection {
   swap(leftIndex: number, rightIndex: number): void {
     const leftHand = this.data[leftIndex];
     this.data[leftIndex] = this.data[rightIndex];
-    this.data[(rightIndex = leftHand)];
+    this.data[rightIndex] = leftHand;
   }
 }
 
