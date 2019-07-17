256acd9402ba29d41792551c332b129a88b09683
Deepak Bhari <deepakbhari@Deepaks-MBP.lan>
2019-07-17 15:48:39 +0100

    number sorting algorithm

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
-console.log('hello');
+class Sorter {
+  //collection: number[];
 
-const logSomething = () => {
-  console.log('ededed');
-};
+  // constructor(public collection: number[]) {
+  //   this.collection = collection;
+  // }
 
-logSomething();
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
