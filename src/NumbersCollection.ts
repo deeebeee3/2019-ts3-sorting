245c1f569f89ee3b310964554a6cf2c554e1ca9b
Deepak Bhari <deepakbhari@Deepaks-MBP.lan>
2019-07-17 17:11:01 +0100

    A much better approach to sorting numbers

---
+++ src/NumbersCollection.ts
@@ -0,0 +1,25 @@
+export class NumbersCollection {
+  //data: number[];
+  // constructor(data: number[]){
+  //   this.data = data;
+  // }
+
+  constructor(public data: number[]) {}
+
+  get length(): number {
+    return this.data.length;
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return this.data[leftIndex] > this.data[rightIndex];
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftHand = this.data[leftIndex];
+    this.data[leftIndex] = this.data[rightIndex];
+    this.data[(rightIndex = leftHand)];
+  }
+}
+
+// const collection = new NumbersCollection([1, 2, 3, 4]);
+// collection.length;
